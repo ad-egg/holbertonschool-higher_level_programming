3a55ccda29bf14ab7d46e2fc25a40fcb1cdb9bfe
Athena Deng <539@holbertonschool.com>
2018-12-05 04:53:53 +0000

    first commit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        num2 = number * -1
+    else:
+        num2 = number
+    lastdigit = num2 % 10
+    print("{:d}".format(lastdigit), end='')
+    return lastdigit
