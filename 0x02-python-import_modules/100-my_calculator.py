d0deb8ebf2b7287cc359fc95e5accad8c724eda7
Athena Deng <539@holbertonschool.com>
2018-12-06 05:02:46 +0000

    changed order of if conditions, convert string to int

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,20 +7,20 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>".format())
         sys.exit(1)
     op = sys.argv[2]
-    if (op != "+") or (op != "-") or (op != "*") or (op != "/"):
-        print("Unknown operator. Available operators: +, -, * and /".format())
-        sys.exit(1)
-    a = sys.argv[1]
-    b = sys.argv[3]
-    if op == "+":
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if op[0] == '+':
         print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
         sys.exit(0)
-    elif op == "-":
+    elif op[0] == '-':
         print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
         sys.exit(0)
-    elif op == "*":
+    elif op[0] == '*':
         print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
         sys.exit(0)
-    elif op == "/":
+    elif op[0] == '/':
         print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
         sys.exit(0)
+    elif (op[0] != '+') or (op[0] != '-') or (op[0] != '*') or (op[0] != '/'):
+        print("Unknown operator. Available operators: +, -, * and /".format())
+        sys.exit(1)
