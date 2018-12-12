2ad5cf67c8bac5bfdf08d23668324aa3f769b4c2
Athena Deng <539@holbertonschool.com>
2018-12-12 02:24:48 +0000

    removed if condition

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if not my_string:
-        return None
     newstring = ""
     for i in range(0, len(my_string)):
         if (my_string[i] == 'c') or (my_string[i] == 'C'):
