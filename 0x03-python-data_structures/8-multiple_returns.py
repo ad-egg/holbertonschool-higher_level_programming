8f4999fb054b692e459126631fcfc7284e7e5568
Athena Deng <539@holbertonschool.com>
2018-12-12 05:07:37 +0000

    set string to None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         strlength = 0
-        sentence[0] = None
+        sentence = None
     else:
         strlength = len(sentence)
     my_tuple = (strlength, sentence[0])
