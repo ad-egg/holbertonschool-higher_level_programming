93336757424f7498a3ac3f2cd274f7593d4a321f
Athena Deng <539@holbertonschool.com>
2018-12-12 03:19:36 +0000

    first commit

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        strlength = 0
+        sentence[0] = ""
+    else:
+        strlength = len(sentence)
+    my_tuple = strlength, sentence[0]
+    return my_tuple
