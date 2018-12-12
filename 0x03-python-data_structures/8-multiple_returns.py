1ddefc11f7439934fbc431ba927a1f357bb12725
Athena Deng <539@holbertonschool.com>
2018-12-12 05:36:08 +0000

    moved duplicate line outside of if/else statement

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    strlength = len(sentence)
     if len(sentence) == 0:
-        strlength = 0
         firstchar = None
     else:
-        strlength = len(sentence)
         firstchar = sentence[0]
     my_tuple = (strlength, firstchar)
     return my_tuple
