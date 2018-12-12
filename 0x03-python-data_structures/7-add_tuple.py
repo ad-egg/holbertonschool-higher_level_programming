2ff56baa885b9f7558e06b2eec8ff52e6d1e8383
Athena Deng <539@holbertonschool.com>
2018-12-12 04:29:19 +0000

    stored values in integer variables

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        tuple_a = (0, 0)
-    elif len(tuple_a) == 1:
-        tuple_a = (tuple_a[0], 0)
-    if len(tuple_b) == 0:
-        tuple_b = (0, 0)
-    elif len(tuple_b) == 1:
-        tuple_b = (tuple_b[0], 0)
-    tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    a0 = 0
+    a1 = 0
+    b0 = 0
+    b1 = 0
+    a0 = tuple_a[0]
+    a1 = tuple_a[1]
+    b0 = tuple_b[0]
+    b1 = tuple_b[1]
+    tuple_c = (a0 + b0, a1 + b1)
     return tuple_c
