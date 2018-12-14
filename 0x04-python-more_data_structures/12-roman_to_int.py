28bd96baeb0e6637e7fad1fbc6121241ee7a0770
Athena Deng <539@holbertonschool.com>
2018-12-14 23:42:32 +0000

    removed dictionary, changed if conditions

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not isinstance(roman_string, str):
         return 0
-    numerals = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     no = 0
     for i in range(0, len(roman_string)):
         if roman_string[i] == 'I':
@@ -13,14 +12,14 @@ def roman_to_int(roman_string):
         elif roman_string[i] == 'V':
             no += 5
         elif roman_string[i] == 'X':
-            if i < len(roman_string) - 1 and roman_string[i + 1] != 'C':
+            if i < len(roman_string) - 1 and roman_string[i + 1] == 'C':
                 no -= 10
             else:
                 no += 10
         elif roman_string[i] == 'L':
             no += 50
         elif roman_string[i] == 'C':
-            if i < len(roman_string) - 1 and roman_string[i + 1] != 'M':
+            if i < len(roman_string) - 1 and roman_string[i + 1] == 'M':
                 no -= 100
             else:
                 no += 100
