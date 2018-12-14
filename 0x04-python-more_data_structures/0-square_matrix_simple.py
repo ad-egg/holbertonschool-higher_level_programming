3b941c466c62064f43b05ff7913f2d3053898271
Athena Deng <539@holbertonschool.com>
2018-12-14 00:34:31 +0000

    added variable

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,8 @@
 def square_matrix_simple(matrix=[]):
     sq_matrix = []
     for i in range(0, len(matrix)):
+        sq_matrix_row = []
         for j in range(0, len(matrix[i])):
-            sq_matrix.append((matrix[i][j] * matrix[i][j]))
+            sq_matrix_row.append(matrix[i][j] * matrix[i][j])
+        sq_matrix.append(sq_matrix_row)
     return sq_matrix
