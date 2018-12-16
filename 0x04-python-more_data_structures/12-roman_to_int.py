ab84ba017381a9cd842abcbe7b1d4dd84fbaa599
Athena Deng <539@holbertonschool.com>
2018-12-16 00:17:23 +0000

    shortened dictionary name to be able to fit two if conditions in one line and still use PEP 8 style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,17 +2,13 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    drom = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    rom = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     no = 0
     for i in range(0, len(roman_string)):
         k = roman_string[i]
-        if k in drom:
-            if i < len(roman_string) - 1:
-                l = roman_string[i + 1]
-                if drom[k] < drom[l]:
-                    no -= drom[k]
-                else:
-                    no += drom[k]
+        if k in rom:
+            if i < len(roman_string) - 1 and rom[k] < rom[roman_string[i + 1]]:
+                no -= rom[k]
             else:
-                no += drom[k]
+                no += rom[k]
     return no
