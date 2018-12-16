a9efbc81b937ba08f7ff4e419ae53ce708a30b4c
Athena Deng <539@holbertonschool.com>
2018-12-16 22:11:12 +0000

    checked for max integer value instead of key value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,11 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    k = sorted(a_dictionary.keys())
-    return k[-1]
+    k = list(a_dictionary.keys())
+    o = sorted(a_dictionary.values())
+    v = list (a_dictionary.values())
+    vhi = o[-1]
+    for no in range(len(v)):
+        if v[no] == vhi:
+            khi = k[no]
+    return khi
