a8c332b58da259fde67669ffaeb401258996d4c4
Athena Deng <539@holbertonschool.com>
2018-12-16 00:10:40 +0000

    added dictionary and shortened entire function

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,29 +2,17 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
+    drom = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     no = 0
     for i in range(0, len(roman_string)):
-        if roman_string[i] == 'I':
-            if i < len(roman_string) - 1 and roman_string[i + 1] != 'I':
-                no -= 1
+        k = roman_string[i]
+        if k in drom:
+            if i < len(roman_string) - 1:
+                l = roman_string[i + 1]
+                if drom[k] < drom[l]:
+                    no -= drom[k]
+                else:
+                    no += drom[k]
             else:
-                no += 1
-        elif roman_string[i] == 'V':
-            no += 5
-        elif roman_string[i] == 'X':
-            if i < len(roman_string) - 1 and roman_string[i + 1] == 'C':
-                no -= 10
-            else:
-                no += 10
-        elif roman_string[i] == 'L':
-            no += 50
-        elif roman_string[i] == 'C':
-            if i < len(roman_string) - 1 and roman_string[i + 1] == 'M':
-                no -= 100
-            else:
-                no += 100
-        elif roman_string[i] == 'D':
-            no += 500
-        elif roman_string[i] == 'M':
-            no += 1000
+                no += drom[k]
     return no
