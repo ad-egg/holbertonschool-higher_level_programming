2693f6fcff3c9846bf93c7730693665340d0b55c
Athena Deng <539@holbertonschool.com>
2018-12-16 22:11:59 +0000

    removed whitespace before '('

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
         return None
     k = list(a_dictionary.keys())
     o = sorted(a_dictionary.values())
-    v = list (a_dictionary.values())
+    v = list(a_dictionary.values())
     vhi = o[-1]
     for no in range(len(v)):
         if v[no] == vhi:
