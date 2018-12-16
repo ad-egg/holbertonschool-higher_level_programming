5d7411a8421a48cb0f30aaa2bea6dca04c10ce66
Athena Deng <539@holbertonschool.com>
2018-12-16 05:06:04 +0000

    removed unnecessary if statement

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    k = sorted(a_dictionary.keys())
-    for i in range(0, len(k)):
-        if k[i] in a_dictionary:
-            print("{}: {}".format(k[i], a_dictionary[k[i]]))
+    for word in sorted(a_dictionary.keys()):
+        print("{}: {}".format(word, a_dictionary[word]))
