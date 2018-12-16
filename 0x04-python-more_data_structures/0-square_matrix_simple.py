dd9143872f0880dcde9014417255e76319d0b153
Athena Deng <539@holbertonschool.com>
2018-12-16 01:28:56 +0000

    did everything in one line

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sq_matrix = []
-    for i in range(0, len(matrix)):
-        sq_matrix_row = []
-        for j in range(0, len(matrix[i])):
-            sq_matrix_row.append(matrix[i][j] * matrix[i][j])
-        sq_matrix.append(sq_matrix_row)
-    return sq_matrix
+    return [[n * n for n in row] for row in matrix]
