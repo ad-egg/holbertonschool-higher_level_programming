28f4c7ca50e66259167ff51b6c3369e2bfae5c94
Athena Deng <539@holbertonschool.com>
2018-12-16 03:37:58 +0000

    reduced number of lines

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     newlist = my_list[:]
-    for item in newlist:
-        if item == search:
-            newlist[newlist.index(item)] = replace
-    return newlist
+    return [replace if search == element else element for element in newlist]
