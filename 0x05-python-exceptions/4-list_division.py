65a53e68d9e16b521fa551b6c4e71af958850e96
Athena Deng <539@holbertonschool.com>
2019-01-03 02:42:43 +0000

    added variable, removed duplicate lines of code

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,17 +2,15 @@
 def list_division(my_list_1, my_list_2, list_length):
     newlist = []
     for i in range(0, list_length):
+        num = 0
         try:
-            newlist.append(my_list_1[i] / my_list_2[i])
+            num = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            newlist.append(0)
             print("{}".format("division by 0"))
         except TypeError:
-            newlist.append(0)
             print("{}".format("wrong type"))
         except IndexError:
-            newlist.append(0)
             print("{}".format("out of range"))
         finally:
-            print("".format(), end="")
+            newlist.append(num)
     return newlist
