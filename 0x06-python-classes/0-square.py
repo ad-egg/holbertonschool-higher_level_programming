60d9194737e232382ebd6c397607f7bef0b9a60c
Athena Deng <539@holbertonschool.com>
2019-01-03 22:40:10 +0000

    first commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""0-square.py
+This module makes an empty class Square that defines a square.
+
+"""
+class Square:
+    """an empty class Square that defines a square
+    a square is a parallelogram
+    """
+    pass
