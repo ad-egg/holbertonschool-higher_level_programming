03a86b86f303628ede2c1bc9d040c02333ba7fe5
Athena Deng <539@holbertonschool.com>
2019-01-03 22:54:25 +0000

    added 2 blank lines for PEP8

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """0-square.py
 This module makes an empty class Square that defines a square.
-
 """
+
+
 class Square:
     """an empty class Square that defines a square
     a square is a parallelogram
