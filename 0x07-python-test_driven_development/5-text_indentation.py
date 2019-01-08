7b0a539984645e1f07c0a927d110fd7df0c0b508
Athena Deng <539@holbertonschool.com>
2019-01-08 22:54:25 +0000

    added loop for string that starts with space(s) or is all spaces

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,6 +13,8 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
     while i < len(text):
         print(text[i], end="")
         if text[i] == '.' or text[i] == '?' or text[i] == ':':
