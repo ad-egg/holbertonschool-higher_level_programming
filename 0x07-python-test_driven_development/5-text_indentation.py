5a7336023ec40867fdda9e123b2f79c15887903c
Athena Deng <539@holbertonschool.com>
2019-01-08 20:53:50 +0000

    changed to while loops

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,18 +10,16 @@ def text_indentation(text):
     This function prints a text with 2 new lines after each period, question
     mark, and colon.
     """
-    stringlist = []
-    mystring = ""
-    for car in text:
-        mystring = "".join(car)
-        if car == '.' or car == '?' or car == ':':
-            stringlist.append(mystring)
-            mystring = ""
-    for lines in stringlist:
-        i = 0
-        while lines[i] == ' ':
-            i += 1
-        while i < len(lines):
-            print(lines[i], end="")
-            i += 1
-        print("\n")
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print("\n")
+            while i < len(text) - 1:
+                if text[i + 1] == ' ':
+                    i += 1
+                else:
+                    break
+        i += 1
