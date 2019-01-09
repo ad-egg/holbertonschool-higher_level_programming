dc532e30861522d329d63c7503a104e95316e9df
Athena Deng <539@holbertonschool.com>
2019-01-09 00:21:04 +0000

    function does not print square when passed float

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,13 +9,8 @@ def print_square(size):
     This function prints a square with the octothorpe character.
     The size of the square depends on the size passed to the function.
     """
-    if type(size) is not int and type(size) is not float:
+    if type(size) is not int:
         raise TypeError("size must be an integer")
-    if type(size) is float:
-        if size < 0:
-            raise TypeError("size must be an integer")
-        else:
-            size = int(size)
     if type(size) is int and size < 0:
         raise ValueError("size must be >= 0")
     for row in range(0, size):
