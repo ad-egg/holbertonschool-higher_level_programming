6155ebe4dfec7d28eb3a713b497426439dbac565
Athena Deng <539@holbertonschool.com>
2019-01-09 20:59:15 +0000

    first commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """an empty class Rectangle that defines a rectangle
+    a rectangle is a parallelogram with four right angles
+    """
+    pass
