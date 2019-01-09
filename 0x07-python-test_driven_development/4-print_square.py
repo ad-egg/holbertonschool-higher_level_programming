c32f5983c9917e5fd2d3f08a992b31b788d1051b
Athena Deng <539@holbertonschool.com>
2019-01-09 00:19:15 +0000

    cast float to int

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -15,7 +15,7 @@ def print_square(size):
         if size < 0:
             raise TypeError("size must be an integer")
         else:
-            int(size)
+            size = int(size)
     if type(size) is int and size < 0:
         raise ValueError("size must be >= 0")
     for row in range(0, size):
