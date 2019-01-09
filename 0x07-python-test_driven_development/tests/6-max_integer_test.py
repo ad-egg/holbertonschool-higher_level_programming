63159db651767317567aa420b493dd526c8c83d3
Athena Deng <539@holbertonschool.com>
2019-01-09 04:05:48 +0000

    added blank lines for pep8

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,6 +4,7 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """
     Test the max_integer function from 6-max_integer module
@@ -14,30 +15,35 @@ class TestMaxInteger(unittest.TestCase):
         """
         nothing = max_integer([])
         self.assertEqual(nothing, None)
+
     def two_numbers(self):
         """
         tests for passing two numbers
         """
         terror = max_integer(1, 2)
         self.assertRaises(TypeError)
+
     def nice_int_list(self):
         """
         tests with a list of ints
         """
         biggest = max_integer([12, 60, 36])
         self.assertEqual(biggest, 60)
+
     def one_string(self):
         """
         passing a string to the function
         """
         highest_ascii = max_integer("asdf")
         self.assertEqual(highest_ascii, s)
+
     def strings_list(self):
         """
         passing a list of strings
         """
         lomgest = max_integer(["egg", "oeuf", "fromage"])
         self.assertEqual(lomgest, fromage)
+
     def infinities(self):
         """
         comparing infinity and negative infinity
@@ -46,6 +52,7 @@ class TestMaxInteger(unittest.TestCase):
         neg_infin = float("-inf")
         big_infinity = max_integer([pos_infin, neg_infin])
         self.assertEqual(big_infinity, pos_infin)
+
     def neg_infinity_to_zero(self):
         """
         comparing negative infinity to 0
@@ -53,5 +60,6 @@ class TestMaxInteger(unittest.TestCase):
         neg_infin = float("-inf")
         zero = max_integer([0, neg_infin])
         self.assertEqual(zero, 0)
+
 if __name__ == '__main__':
     unittest.main()
