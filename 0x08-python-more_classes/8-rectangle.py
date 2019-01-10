f40b0f60af1ed8721a8cfa743ca61bc733b1932a
Athena Deng <539@holbertonschool.com>
2019-01-10 05:21:09 +0000

    corrected keyword

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -106,9 +106,9 @@ class Rectangle:
         if they have the same area, the first rectangle is returned
         """
         if not isinstance(rect_1, Rectangle):
-            Raise TypeError("rect_1 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
-            Raise TypeError("rect_2 must be an instance of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
             return rect_1
         else:
