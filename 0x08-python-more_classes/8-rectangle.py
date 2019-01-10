b578951c0704399941210ef7da48564c1789a681
Athena Deng <539@holbertonschool.com>
2019-01-10 05:17:26 +0000

    checked for instances of Rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -105,8 +105,10 @@ class Rectangle:
         compares two rectangles and returns the one with larger area
         if they have the same area, the first rectangle is returned
         """
-        if rect_2 == None:
-            return rect_1
+        if not isinstance(rect_1, Rectangle):
+            Raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            Raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
             return rect_1
         else:
