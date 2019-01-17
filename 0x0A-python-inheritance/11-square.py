84f74400654037c8280ced92071a78daa1b75821
Athena Deng <539@holbertonschool.com>
2019-01-17 05:10:31 +0000

    first commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+this module contains class Square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square inherits from class Rectangle
+    """
+    def __init__(self, size):
+        """
+        instantiates a Square with size
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        self._Rectangle__width = size
+        self._Rectangle__height = size
+
+    def area(self):
+        """
+        returns area of a Square
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        returns description of a Square instance
+        """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
