78d1a8849c118d3b1187103b4aefdfd90d4a3619
Athena Deng <539@holbertonschool.com>
2019-01-18 01:19:24 +0000

    first commit

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+this module contains a function that writes a string to a text file and
+returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    this function writes a string to a text file (UTF8) and returns
+    the number of characters written
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
