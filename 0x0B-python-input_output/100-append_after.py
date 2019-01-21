55a57e97ab0bed81bc86857cc3b3f135878561e9
Athena Deng <539@holbertonschool.com>
2019-01-21 04:08:23 +0000

    shortened code

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,18 +14,11 @@ def append_after(filename="", search_string="", new_string=""):
         len_targetstr = len(search_string)
         for y in range(len(lines)):
             line = lines[y]
-            if len(line) > len_targetstr:  # line may have target str
-                for x in range(len(line) - len_targetstr + 1):
-                    if line[x] == search_string[0]:  # matches first letter
-                        for i in range(1, len_targetstr):
-                            if line[x + i] != search_string[i]:  # not a match
-                                break
-                            if i == len_targetstr - 1 and line[
-                                 x + i] == search_string[i]:
-                                if y < len(lines) - 1:
-                                    lines[y + 1] = new_string + lines[y + 1]
-                                else:
-                                    lines.append(new_string)
+            if search_string in line:
+                if y < len(lines) - 1:
+                    lines[y + 1] = new_string + lines[y + 1]
+                else:
+                    lines.append(new_string)
     with open(filename, 'w') as egg:
         for line in lines:
             egg.write(line)
