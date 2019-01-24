e15a8ccfeb2e5db6932a663ee13f9734bf301637
Athena Deng <539@holbertonschool.com>
2019-01-24 01:53:42 +0000

    changed if conditions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -144,15 +144,15 @@ class Rectangle(Base):
         """
         assigns an argument to each attribute
         """
-        if len(args) > 1:
+        if len(args) > 0:
             self.id = args[0]
-        if len(args) > 2:
+        if len(args) > 1:
             self.width = args[1]
-        if len(args) > 3:
+        if len(args) > 2:
             self.height = args[2]
-        if len(args) > 4:
+        if len(args) > 3:
             self.x = args[3]
-        if len(args) >= 5:
+        if len(args) > 4:
             self.y = args[4]
         if args is None or len(args) == 0:
             for key, value in kwargs.items():
