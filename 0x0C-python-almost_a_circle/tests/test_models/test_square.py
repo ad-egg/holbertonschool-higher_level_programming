cc1e824fac4fc0f403cf08346fb99cc4aa40d39e
Athena Deng <539@holbertonschool.com>
2019-01-27 03:43:12 +0000

    added tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -10,3 +10,122 @@ class TestClassBase(unittest.TestCase):
     """
     test the class Square
     """
+
+    def test_no_args(self):
+        """
+        test for what happens if did not pass any arguments
+        """
+        with self.assertRaises(TypeError):
+            empty = Square()
+
+    def setUp(self):
+        """
+        this is for testing
+        """
+        self.equad = Square(6, 6, 6, 6)
+
+    def test_set_id(self):
+        """
+        tests if the id was instantiated correctly
+        """
+        self.assertEqual(self.equad.id, 6)
+
+    def test_size(self):
+        """
+        tests if width was instantiated correctly
+        """
+        self.assertEqual(self.equad.size, 6)
+
+    def test_x(self):
+        """
+        tests if x was instantiated correctly
+        """
+        self.assertEqual(self.equad.x, 6)
+
+    def test_y(self):
+        """
+        tests if y was instantiated correctly
+        """
+        self.assertEqual(self.equad.y, 6)
+
+    def test_get_set_size(self):
+        """
+        tests getter/setter for size
+        """
+        self.equad.size = 9
+        self.assertEqual(self.equad.size, 9)
+
+    def test_get_set_x(self):
+        """
+        tests getter/setter for x
+        """
+        self.equad.x = 9
+        self.assertEqual(self.equad.x, 9)
+
+    def test_get_set_y(self):
+        """
+        tests getter/setter for y
+        """
+        self.equad.y = 9
+        self.assertEqual(self.equad.y, 9)
+
+    def test_update_args(self):
+        """
+        tests the update public method with *args
+        """
+        self.equad.update(8, 8, 8, 8)
+        self.assertEqual(self.equad.id, 8)
+        self.assertEqual(self.equad.size, 8)
+        self.assertEqual(self.equad.x, 8)
+        self.assertEqual(self.equad.y, 8)
+
+    def test_update_kwargs_id(self):
+        """
+        test updating the instance id with **kwargs
+        """
+        self.equad.update(id=6)
+        self.assertEqual(self.equad.id, 6)
+
+    def test_update_kwargs_size(self):
+        """
+        test updating the instance width with **kwargs
+        """
+        self.equad.update(size=6)
+        self.assertEqual(self.equad.size, 6)
+
+    def test_update_kwargs_x(self):
+        """
+        test updating the instance x with **kwargs
+        """
+        self.equad.update(x=6)
+        self.assertEqual(self.equad.x, 6)
+
+    def test_update_kwargs_y(self):
+        """
+        test updating the instance y with **kwargs
+        """
+        self.equad.update(y=6)
+        self.assertEqual(self.equad.y, 6)
+
+    def test_set_size_not_int(self):
+        """
+        test setting width with non-int
+        """
+        with self.assertRaises(TypeError):
+            self.equad.size = "size"
+
+    def test_set_size_neg_int(self):
+        """
+        test setting width with negative integer
+        """
+        with self.assertRaises(ValueError):
+            self.equad.size = -5
+
+    def test_area(self):
+        """
+        test public method that returns area of instance
+        """
+        self.assertEqual(self.equad.area(), 36)
+
+if __name__ == '__main__':
+    unittest.main()
