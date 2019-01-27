bc63b34e1c63849545ecd83e05e3fede0c710506
Athena Deng <539@holbertonschool.com>
2019-01-27 00:19:24 +0000

    added 3 unittests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -10,4 +10,26 @@ class TestClassBase(unittest.TestCase):
     """
     test the class Base
     """
-    def test_
+    def test_create_instance_w_int_id(self):
+        """
+        tests for id of an instance with int id
+        """
+        b12 = Base(12)
+        self.assertEqual(b12.id, 12)
+
+    def test_create_instance_w_nint_id(self):
+        """
+        tests for id of an instance with non-int id
+        """
+        pi_base = Base(3.14)
+        self.assertEqual(pi_base.id, 3.14)
+        
+    def test_create_instance_no_id(self):
+        """
+        tests for id of an instance that did not receive an id
+        """
+        first = Base()
+        self.assertEqual(first.id, 1)
+
+if __name__ == '__main__':
+    unittest.main()
