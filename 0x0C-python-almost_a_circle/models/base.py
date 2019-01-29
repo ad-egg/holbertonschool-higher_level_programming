660ad869d22f1a1f0c01e1e3cc9057d80e1f645a
Athena Deng <539@holbertonschool.com>
2019-01-29 05:03:05 +0000

    added if condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,7 +29,10 @@ class Base:
         """
         returns the JSON string representation of list_dictionaries
         """
-        return json.dumps(list_dictionaries)
+        if list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
