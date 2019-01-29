337c6da906d65f5635dbca91c0d948cc9bd626d0
Athena Deng <539@holbertonschool.com>
2019-01-29 05:38:45 +0000

    reverted to older version

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -64,10 +64,7 @@ class Base:
         """
         returns an instance with all attributes already set
         """
-        if cls == "Rectangle":
-            dummy = Rectangle(6, 6, 6)
-        elif cls == "Square":
-            dummy = Square(6, 6)
+        dummy = cls(6, 6, 6)
         dummy.update(**dictionary)
         return dummy
 
