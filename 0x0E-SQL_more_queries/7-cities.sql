bd487e842318bc3c0a9de618aefc508b5272979e
Athena Deng <539@holbertonschool.com>
2019-03-06 01:37:19 +0000

    first commit

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,8 @@
 -- creates database hbtn_0d_usa and table cities in it
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.`cities` (
-		`id` INT UNIQUE AUTO_INCREMEMENT NOT NULL PRIMARY KEY,
-		`state_id` INT NOT NULL FOREIGN KEY REFERENCES states(id),
-		`name` VARCHAR(256) NOT NULL);
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `cities` (
+		`id` INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+		`state_id` INT NOT NULL,
+		`name` VARCHAR(256) NOT NULL,
+		FOREIGN KEY (`state_id`) REFERENCES `states` (`id`));
