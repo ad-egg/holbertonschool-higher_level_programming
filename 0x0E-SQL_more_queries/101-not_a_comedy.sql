cea4ee7c6b237c219fdedd7cd4402529fdfe7198
Athena Deng <539@holbertonschool.com>
2019-03-07 06:17:48 +0000

    corrected typo

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -6,7 +6,7 @@ WHERE title NOT IN (
 		SELECT title FROM tv_shows
 		INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 		INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
-		WHERE title='Comedy'
+		WHERE name='Comedy'
 )
 GROUP BY title
 ORDER BY title ASC;
