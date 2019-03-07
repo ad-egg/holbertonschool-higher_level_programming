220a133613529e6e2d7fb2180aa762d68de36f13
Athena Deng <539@holbertonschool.com>
2019-03-07 00:34:19 +0000

    completed task

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,8 @@
 -- list all genres of the show Dexter
--- SELECT tv_genres.name
--- FROM 
-
--- ORDER BY tv_genres.name ASC;
-
-
-
-
--- SELECT id FROM tv_shows WHERE title='Dexter'
-
+SELECT name FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id=tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_shows.id=tv_show_genres.show_id
+WHERE title='Dexter'
+ORDER BY tv_genres.name ASC;
