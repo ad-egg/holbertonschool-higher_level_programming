0cf1ed9f6960e08bb8becc7f191e7917cb902792
Athena Deng <539@holbertonschool.com>
2019-03-07 06:41:05 +0000

    specified which table the column is from

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,11 +2,11 @@
 SELECT title FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
-WHERE title NOT IN (
+WHERE tv_shows.title NOT IN (
 		SELECT title FROM tv_shows
 		INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 		INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
 		WHERE name='Comedy'
 )
-GROUP BY title
-ORDER BY title ASC;
+GROUP BY tv_shows.title
+ORDER BY tv_shows.title ASC;
