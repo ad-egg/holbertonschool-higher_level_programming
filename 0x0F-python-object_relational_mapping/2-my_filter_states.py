ca500da2d70259a09816b64afaaf50b19d4a0155
Athena Deng <539@holbertonschool.com>
2019-03-29 03:26:39 +0000

    added single quotes, broke up long line for pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
             passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(
-            "SELECT * FROM states WHERE name = {}".format(argv[4]))
+            "SELECT * FROM states WHERE name = '{}' \
+            ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
     db.close()
     for row in rows:
