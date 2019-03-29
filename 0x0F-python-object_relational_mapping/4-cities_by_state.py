9b9c73487063305ebbe65b2dc1684b0c7c6a69c5
Athena Deng <539@holbertonschool.com>
2019-03-29 23:53:49 +0000

    changed order of variables

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
             host="localhost", port=3306, user=argv[1],
             passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id,states.name,cities.name FROM cities \
+    cur.execute("SELECT cities.id,cities.name,states.name FROM cities \
                 LEFT JOIN states ON cities.state_id=states.id \
                 ORDER BY cities.id ASC")
     rows = cur.fetchall()
