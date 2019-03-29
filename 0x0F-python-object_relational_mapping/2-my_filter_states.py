46843172e5d040c54cba80205de5da8b1e448e82
Athena Deng <539@holbertonschool.com>
2019-03-29 23:49:29 +0000

    added case sensitivity

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
             passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute(
-            "SELECT * FROM states WHERE name = '{}' \
+            "SELECT * FROM states \
+            WHERE name = '{}' COLLATE latin1_general_cs \
             ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
     db.close()
