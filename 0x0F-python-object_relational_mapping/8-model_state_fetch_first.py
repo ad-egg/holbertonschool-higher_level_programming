e67a8272c136f739d77201e10a2acfa49979cfb6
Athena Deng <539@holbertonschool.com>
2019-03-30 22:06:46 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+prints the first State object from the database
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy.orm import Session
+    from sqlalchemy import (create_engine)
+
+    user = argv[1]
+    passw = argv[2]
+    db_name = argv[3]
+
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(user, passw, db_name),
+            pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).first()
+    session.close()
+
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
