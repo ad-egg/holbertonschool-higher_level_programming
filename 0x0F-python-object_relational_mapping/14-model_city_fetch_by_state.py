83ddf5212e6dc99ca10a1ecf453ba0907c0718b8
Athena Deng <539@holbertonschool.com>
2019-03-31 03:04:36 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""prints all City objects from a database"""
+
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import Session
+    from sys import argv
+
+    user = argv[1]
+    passw = argv[2]
+    db_name = argv[3]
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(user, passw, db_name),
+            pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    joined = session.query(City, State).filter(City.state_id == State.id).order_by(City.id.asc()).all()
+    session.close()
+
+    for cities, states in joined:
+        print("{}: ({:d}) {}".format(states.name, cities.id, cities.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,15 +2,17 @@
 """the class definition of a State an an instance Base = declarative_base()"""
 
 
+from model_state import State
+from sqlalchemy import ForeignKey
 from sqlalchemy import Table, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
-
 
 class City(Base):
     """a class that inherits from Base"""
     __tablename__ = "cities"
     id = Column(Integer, nullable=False, unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
+    state_id = Column(Integer, ForeignKey('states.id'))
