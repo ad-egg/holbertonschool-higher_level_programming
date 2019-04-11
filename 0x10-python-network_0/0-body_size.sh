03f7fd03a79e41459f75e431583fa066b074196a
Athena Deng <539@holbertonschool.com>
2019-04-11 00:26:09 +0000

    added double quotes to prevent globbing and word splitting

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes a URL, sends a request, and returns the size of the body
-curl -w '%{size_download}\n' -so /dev/null $1
+curl -w '%{size_download}\n' -so /dev/null "$1"
