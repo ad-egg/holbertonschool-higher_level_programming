a462a4f2b5c8044076c5a833eae5639d42e04ba0
Athena Deng <539@holbertonschool.com>
2019-04-11 02:36:42 +0000

    removed equal sign

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,14 +11,14 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 1:
         return list_of_integers[0]
     # see if the ends are peaks
-    if list_of_integers[0] >= list_of_integers[1]:
+    if list_of_integers[0] > list_of_integers[1]:
         return list_of_integers[0]
-    elif list_of_integers[-1] >= list_of_integers[-2]:
+    elif list_of_integers[-1] > list_of_integers[-2]:
         return list_of_integers[-1]
     # peak is not at either end
     else:
         middle = len(list_of_integers) // 2
-        if list_of_integers[middle] < list_of_integers[middle + 1]:
+        if list_of_integers[middle] <= list_of_integers[middle + 1]:
             return find_peak(list_of_integers[middle + 1:])
         else:
             return find_peak(list_of_integers[:middle - 1])
