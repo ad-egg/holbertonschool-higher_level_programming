be5a6bbef2df466c57b957dcfc9106461434dd6a
Athena Deng <539@holbertonschool.com>
2019-04-11 03:14:24 +0000

    took out option for fetching HTTP header only

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes a URL then sends it a request and displays only the status code of the response
-curl "$1" -sIw "%{http_code}" -o /dev/null
+curl "$1" -w "%{http_code}" -so /dev/null
