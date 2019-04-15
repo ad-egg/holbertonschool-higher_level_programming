91f56625cfda20b88bcfd7088f34e4dfa02c3bdf
Athena Deng <539@holbertonschool.com>
2019-04-15 23:09:09 +0000

    everything in with statements

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,8 +6,7 @@ takes a URL, sends a request, and displays the X-Request-Id
 import urllib.request
 from sys import argv
 
-req = urllib.request.Request(argv[1])
-
-with urllib.request.urlopen(req) as response:
-    meta_info = dict(response.info())
-    print(meta_info.get('X-Request-Id'))
+with urllib.request.Request(argv[1]) as req:
+    with urllib.request.urlopen(req) as response:
+        meta_info = dict(response.info())
+        print(meta_info.get('X-Request-Id'))
