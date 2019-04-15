04632eac5c967efa7e292e7eaa6b8e3662ee3504
Athena Deng <539@holbertonschool.com>
2019-04-15 22:32:39 +0000

    sends a request to the URL

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,8 @@ takes a URL, sends a request, and displays the X-Request-Id
 import urllib.request
 from sys import argv
 
-with urllib.request.urlopen(argv[1]) as response:
+req = urllib.request.Request(argv[1])
+
+with urllib.request.urlopen(req) as response:
     headers = dict(response.info())
     print(headers.get('X-Request-Id'))
