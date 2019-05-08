35372739ed4f9b7697e12f845b1dea394213c288
Athena Deng <539@holbertonschool.com>
2019-05-08 00:45:44 +0000

    removed optional argument from slice()

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // prints the second biggest integer in list of arguments
 const args = process.argv;
-if (args.length <= 3) { console.log('0'); } else { let sortedValues = args.slice(2, args.length).sort(); console.log(sortedValues[sortedValues.length - 2]); }
+if (args.length <= 3) { console.log('0'); } else { let sortedValues = args.slice(2).sort(); console.log(sortedValues[sortedValues.length - 2]); }
