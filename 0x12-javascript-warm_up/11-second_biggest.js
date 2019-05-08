27963d1270e9581a3a1b72a0afe179147e3eaf24
Athena Deng <539@holbertonschool.com>
2019-05-08 02:29:47 +0000

    changed from const to let

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // prints the second biggest integer in list of arguments
-const args = process.argv.slice(2);
+let args = process.argv.slice(2);
 if (args.length <= 1) {
   console.log('0');
 } else {
