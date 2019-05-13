4b297797a903843792677dbf6a4fdbc288501419
Athena Deng <539@holbertonschool.com>
2019-05-13 20:06:26 +0000

    updated comment

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// empty class Rectangle that defines a rectangle
+// a class Rectangle that defines a rectangle
 
 module.exports = class Rectangle {
   constructor (w, h) { this.width = w; this.height = h; }
