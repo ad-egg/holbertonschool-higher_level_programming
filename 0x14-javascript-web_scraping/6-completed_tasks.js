4f42d18ee515f11675d6b58df15c8f80874e07b4
Athena Deng <539@holbertonschool.com>
2019-05-15 02:09:17 +0000

    first commit

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// computes the number of tasks completed by user id
+const args = process.argv;
+const req = require('request');
+let url = args[2];
+req(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let tasks = JSON.parse(body);
+    let answer = {};
+    for (let task in tasks) {
+      if (task.completed === true) {
+        if (answer[task.userId] == null) {
+          answer[task.userId] = 1;
+        } else {
+          answer[task.userId]++;
+        }
+      }
+    }
+  }
+});
