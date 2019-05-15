a89b92cf5a93e32690cf1b289bb338a9a9a49a52
Athena Deng <539@holbertonschool.com>
2019-05-15 00:12:22 +0000

    first commit

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// prints the title of a Star Wars movie where the episode number matches a given integer
+const args = process.argv;
+const req = require('request');
+let episode = args[2];
+let url = 'http://swapi.co/api/films/' + episode
+req(url, function (error, response, body) {
+  if (error) { console.log(error);
+  } else { 
+  console.log(JSON.parse(body).title);
+  }
+} );
