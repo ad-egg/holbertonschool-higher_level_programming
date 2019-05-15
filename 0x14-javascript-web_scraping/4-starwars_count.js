7074f7bc9a4f15a0ab47e7d33a3d299de6c982e5
Athena Deng <539@holbertonschool.com>
2019-05-15 00:59:13 +0000

    parse results

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,10 @@ const args = process.argv;
 const req = require('request');
 let url = args[2];
 req(url, function (error, response, body) {
-  if (error) { console.log(error);
+  if (error) {
+    console.log(error);
   } else {
-    let films = JSON.parse(body);
+    let films = JSON.parse(body).results;
     let num = 0;
     for (let i = 0; i < films.length; i++) {
       for (let j = 0; j < films[i].characters.length; j++) {
