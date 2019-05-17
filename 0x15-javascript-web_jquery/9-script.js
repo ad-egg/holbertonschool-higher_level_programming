ea2eb5856831e40369d09fbcf1118fe6254a1e11
Athena Deng <539@holbertonschool.com>
2019-05-17 03:28:32 +0000

    ready event

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,3 +1,5 @@
-$.get('http://fourtonfish.com/hellosalut/?mode=auto', function (data) {
-  $('div#hello').text(data.hello)
+$(document).ready(function() {
+  $.get('http://fourtonfish.com/hellosalut/?mode=auto', function (data) {
+    $('div#hello').text(data.hello)
+  });
 });
